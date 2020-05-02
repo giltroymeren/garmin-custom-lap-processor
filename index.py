6665c36623e860f5256882fc49ec521a3b81150b
Giltroy Meren <giltroymeren@gmail.com>
2020-05-02 17:05:15 +0800

    Added command line arguments for file name

--- index.py
+++ index.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
-
 import sys
 import pandas as pandas
 
-source = pandas.read_csv('activity_4853127627.csv')
-to_keep = ['Split','Time','Distance','Avg Speed','Avg HR','Avg Bike Cadence','Normalized Power','Avg Power']
+source_folder = '~/Downloads/'
+activity = str(sys.argv[1])
+source = pandas.read_csv(source_folder + 'activity_' + activity + '.csv')
+to_keep = ['Split','Time','Distance','Avg Speed','Avg HR','Avg Bike Cadence',\
+    'Normalized Power','Avg Power']
 
 for column in ['Avg HR','Avg Bike Cadence','Normalized Power','Avg Power']:
    source[column] = source[column].replace('--', 0)
-   source[column] = pandas.to_numeric(source[column], downcast ='integer', errors='ignore')
+   source[column] = pandas.to_numeric(source[column], downcast ='integer',\
+        errors='ignore')
 
 destination = source[to_keep]
-destination.to_csv('converted.csv')
+destination.to_csv(source_folder + 'converted.csv')
+
+print('Successfully converted activity ' + activity)
